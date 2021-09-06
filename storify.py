6fb472c66ea68ee037dd10dbc150e3c0537274a9
DimalJay <80914328+DimalJay@users.noreply.github.com>
2021-09-06 22:35:44 +0530

    Update storify.py

--- storify.py
+++ storify.py
@@ -8,7 +8,7 @@ def getContent(soup):
     content = ""
     parg = soup.findAll("p")
     for p in parg:
-        content += p.text.strip()+"\n\n"
+        content += p.text.strip()+"\n"
     return content
 
 def getTitle(soup):
